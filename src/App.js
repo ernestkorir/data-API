293dfffb2d1f74347d21ddb03022434e108de422
ernest korir <ernestbore3@gmail.com>
2023-05-25 11:09:22 +0300

    Able to fetch data and display from the API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './components/Homepage';
+import NavBar from './components/Navbar';
 import './App.css';
+import CompanyProfile from './components/CompanyProfile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/company/:name" element={<CompanyProfile />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/CompanyButton.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BiRightArrowAlt } from 'react-icons/bi';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getCompanyProfileAsync } from '../redux/companies/companiesSlice';
+// import './styles/CompaniesList.css';
+
+const CompanyButton = ({ name, rank }) => {
+  const dispatch = useDispatch();
+
+  const handleCompanyProfile = () => {
+    dispatch(getCompanyProfileAsync(name));
+  };
+
+  return (
+    <Link
+      to={`/company/${name}`}
+      className="company-link list-item"
+      onClick={handleCompanyProfile}
+    >
+      <span className="company-symbol">{rank}</span>
+      <span className="company-name">
+        {name}
+        <BiRightArrowAlt />
+      </span>
+    </Link>
+  );
+};
+
+CompanyButton.propTypes = {
+  name: PropTypes.string.isRequired,
+  rank: PropTypes.string.isRequired,
+};
+
+export default CompanyButton;
