ed50bf202b7f3d1c7ba88b984521a413ca973288
ernest korir <ernestbore3@gmail.com>
2023-05-25 10:32:19 +0300

    Add Companiesslice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,10 @@
-const store =() => {
+import { configureStore } from '@reduxjs/toolkit';
+import companiesReducer from './companies/companiesSlice';
 
-}
+const store = configureStore({
+  reducer: {
+    company: companiesReducer,
+  },
+});
 
 export default store;
